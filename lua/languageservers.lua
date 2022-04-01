c653c51940ecd1cb5547a46f785589b09f8abf65
Arthur Soulié <arthur.soulie@tuta.io>
2022-04-01 11:27:46 +0200

    Add eslint language server
    I just understood I didn't need flow lsp since
    typescript lsp can (and is probably better at) type
    checking and autocompletion.

--- lua/languageservers.lua
+++ lua/languageservers.lua
@@ -7,6 +7,7 @@ function languageservers.setup (cap)
     require'lspconfig'.tsserver.setup{cap}
     require'lspconfig'.rls.setup{cap}
     require'lspconfig'.cmake.setup{cap}
+    require'lspconfig'.eslint.setup{cap}
 end
 
 return languageservers
